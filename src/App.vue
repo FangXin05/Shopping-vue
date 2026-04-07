6ab995af099a43aed42abc3bd6cbbbcbe81aa9cf
FangXin <fangxin246@gmail.com>
2026-04-07 17:23:23 +0800

    导入组件库

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
 <template>
   <div id="app">
-    <router-view/>
+    <!-- 测试代码 -->
+     <van-button type="primary">主要按钮</van-button>
+     <van-button type="info">信息按钮</van-button>
+     <van-button type="default">默认按钮</van-button>
+      <router-view/>
+      <van-switch v-model="checked" />
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      checked: false
+    }
+  }
+}
+</script>
 
 <style lang="less">
 
--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import Vant from 'vant'
-import 'vant/lib/index.css'
-
-Vue.use(Vant)
-
+// import Vant from 'vant'
+// import 'vant/lib/index.css'
+//  插件安装初始化
+// Vue.use(Vant)
+// 按需导入vant组件库
+import { Button, Switch } from 'vant'
+Vue.use(Button)
+Vue.use(Switch)
 Vue.config.productionTip = false
 
 new Vue({
